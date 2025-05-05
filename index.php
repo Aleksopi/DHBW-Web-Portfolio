a3993c6fd6c81b0d78cd010bc5d22491cf157a5e
Aleksox <58642974+Aleksopi@users.noreply.github.com>
2025-05-05 23:35:36 +0200

    test

--- index.php
+++ index.php
@@ -10,12 +10,21 @@
 <body>
   <?php include_once('./inc/nav.inc.php'); ?>
   <section class="hero">
-    <div class="hero-image">
-      <img src="./assets/hero.jpg" alt="Hero Image" />
-      <div class="hero-content">
-        <h1>Willkommen auf unserer Webseite!</h1>
-      </div>
+    <div class="hero-image-wrapper">
+      <img src="./assets/img/picture/738D596E-744E-495D-A19B-791236156C0F_1_105_c.jpeg" alt="Hero Image" class="hero-image" />
+    </div>
+    <div class="hero-content">
+      <h1>Willkommen auf unserer Webseite!</h1>
     </div>
   </section>
+  <section class="content">
+    <h2>Über uns</h2>
+    <p>Hier ist ein bisschen Text über uns.</p>
+    <p>Wir freuen uns, dass Sie hier sind!</p>
+    <p>Hier ist ein bisschen Text über uns.</p>
+    <p>Wir freuen uns, dass Sie hier sind!</p>
+    <p>Hier ist ein bisschen Text über uns.</p>
+    <p>Wir freuen uns, dass Sie hier sind!</p>
+
 </body>
 </html>
