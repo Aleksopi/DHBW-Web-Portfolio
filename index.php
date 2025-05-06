66d832f0fdfff4f61e2ab0b56ca77e0d70fbaf94
Aleksox <58642974+Aleksopi@users.noreply.github.com>
2025-05-06 11:09:50 +0200

    update content

--- index.php
+++ index.php
@@ -24,21 +24,23 @@
   </section>
 
   <section class="highlight animate-on-scroll">
-  <div class="highlight-container reverse-layout">
-    <div class="highlight-text slide-left">
-      <h2>
-        Ferrari F40:<br>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      </h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur itaque impedit...
-      </p>
-      <a href="#" class="read-more">Mehr lesen <span>&#10140;</span></a>
+    <div class="highlight-container reverse-layout">
+      <div class="highlight-text slide-left">
+        <h2 id="highlight-title">Ferrari F40:<br>Lorem ipsum dolor sit amet.</h2>
+        <p id="highlight-description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur itaque impedit...
+        </p>
+        <a href="#" class="read-more">Mehr lesen <span>&#10140;</span></a>
+      </div>
+      <div class="highlight-image slide-right">
+        <img id="highlight-image" src="./assets/img/picture/7EADC893-9E88-43A8-A091-306E9C3C639E_1_105_c.jpeg" alt="Highlight Image" loading="lazy" />
+        <div class="slider-controls">
+          <button id="prev-slide">&#8592;</button>
+          <button id="next-slide">&#8594;</button>
+        </div>
+      </div>
     </div>
-    <div class="highlight-image slide-right">
-      <img src="./assets/img/picture/7EADC893-9E88-43A8-A091-306E9C3C639E_1_105_c.jpeg" alt="Ferrari F50" loading="lazy" />
-    </div>
-  </div>
+  </section>
   <script>
     const observer = new IntersectionObserver((entries) => {
       entries.forEach(entry => {
@@ -52,9 +54,49 @@
     document.querySelectorAll('.animate-on-scroll').forEach(el => {
       observer.observe(el);
     });
+
+    const slides = [
+      {
+        title: "Ferrari F40:<br>Eine Motorsport-Ikone.",
+        description: "Der Ferrari F40 ist bekannt für seine rohe Leistung und ikonische Form.",
+        image: "./assets/img/picture/7EADC893-9E88-43A8-A091-306E9C3C639E_1_105_c.jpeg"
+      },
+      {
+        title: "Noch ein Ferrari F40:<br>Eine Motorsport-Ikone.",
+        description: "Auch ein Auto",
+        image: "./assets/img/picture/6EC25E8D-CD8C-4B5C-8A46-72E7BDA1825B_1_105_c.jpeg"
+      },
+      {
+        title: "Und noch ein Ferrari F40:<br>Eine Motorsport-Ikone.",
+        description: "Auto macht brum brum",
+        image: "./assets/img/picture/F349224B-F562-489B-B149-988FF6143689_1_105_c.jpeg"
+      }
+    ];
+
+    let currentSlide = 0;
+
+    const titleEl = document.getElementById("highlight-title");
+    const descEl = document.getElementById("highlight-description");
+    const imgEl = document.getElementById("highlight-image");
+
+    function updateSlide(index) {
+      const slide = slides[index];
+      titleEl.innerHTML = slide.title;
+      descEl.textContent = slide.description;
+      imgEl.src = slide.image;
+    }
+
+    document.getElementById("prev-slide").addEventListener("click", () => {
+      currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+      updateSlide(currentSlide);
+    });
+
+    document.getElementById("next-slide").addEventListener("click", () => {
+      currentSlide = (currentSlide + 1) % slides.length;
+      updateSlide(currentSlide);
+    });
   </script>
 
-  </section>
 
 
 
