3afb398482d9cd4aff0b45a19b2fed075a890bf7
Aleksox <58642974+Aleksopi@users.noreply.github.com>
2025-05-06 10:00:42 +0200

    add content section

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
 </head>
 <body>
   <?php include_once('./inc/nav.inc.php'); ?>
+
   <section class="home">
     <div class="home-image-wrapper">
       <img src="./assets/img/picture/738D596E-744E-495D-A19B-791236156C0F_1_105_c.jpeg" alt="Home Image" class="home-image" loading="lazy" />
@@ -21,9 +22,36 @@
       </div>
     </div>
   </section>
-  <section class="content">
-  </section>
-<footer>
-</footer>
+
+  <section class="highlight animate-on-scroll">
+    <div class="highlight-container">
+      <div class="highlight-text slide-left">
+        <h2>Ferrari F40:<br>Lorem ipsum dolor sit amet...</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit...</p>
+        <a href="#" class="read-more">Mehr lesen <span>&#10140;</span></a>
+      </div>
+      <div class="highlight-image slide-right">
+        <img src="./assets/img/picture/7EADC893-9E88-43A8-A091-306E9C3C639E_1_105_c.jpeg" alt="Ferrari F50" loading="lazy" />
+      </div>
+    </div>
+    <script>
+      const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add('animate-in');
+            observer.unobserve(entry.target); // Nur einmal animieren
+          }
+        });
+      }, { threshold: 0.3 });
+
+      document.querySelectorAll('.animate-on-scroll').forEach(el => {
+        observer.observe(el);
+      });
+  </script>
+</section>
+
+
+
+  <footer></footer>
 </body>
 </html>
