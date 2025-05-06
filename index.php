b2a3ffa82865bdb22bbecfb9382785334292bff6
Aleksox <58642974+Aleksopi@users.noreply.github.com>
2025-05-06 13:41:19 +0200

    add fonts

--- index.php
+++ index.php
@@ -4,9 +4,11 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Portfolio</title>
+  <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@500;700&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="./style/nav.css" />
   <link rel="icon" href="./assets/img/logo/logoc.png" type="image/x-icon">
   <link rel="stylesheet" href="./style/style.css" />
+  
 </head>
 <body>
   <?php include_once('./inc/nav.inc.php'); ?>
