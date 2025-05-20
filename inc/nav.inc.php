29fafa4bca2f9ecca60f84ea95e543f4eed05cf9
mario1870 <marioraach01@gmail.com>
2025-05-20 10:47:20 +0200

    updated navbar hamburger on mobile

--- inc/nav.inc.php
+++ inc/nav.inc.php
@@ -1,37 +1,37 @@
 <nav class="navbar">
-    <div class="navbar-container">
-      <div class="navbar-content">
-        <!-- Logo -->
-        <div class="navbar-logo">
-          <a href="./index.php"><img src="./assets/img/picture/66209283-c854-480c-b2dc-cfacf2733bd8.png" alt="Logo"  /></a>
-        </div>
-
-        <!-- Desktop Links -->
-        <div class="nav-links">
-          <a href="./index.php">Home</a>
-          <a href="./gallary.php">Cars</a>
-          <a href="./about.php">About</a>
-          <a href="./kontakt.php">Kontakt</a>
-        </div>
-
-        <!-- Mobile Menu Button -->
-        <div class="mobile-menu-btn">
-          <button id="menu-btn">
-            <svg class="menu-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                d="M4 6h16M4 12h16M4 18h16" />
-            </svg>
-          </button>
-        </div>
+  <div class="navbar-container">
+    <div class="navbar-content">
+      <!-- Logo -->
+      <div class="navbar-logo">
+        <a href="./index.php"><img src="./assets/img/picture/66209283-c854-480c-b2dc-cfacf2733bd8.png" alt="Logo" /></a>
       </div>
-    </div>
 
-    <!-- Mobile Menu -->
-    <div id="mobile-menu" class="mobile-menu collapsed">
+      <!-- Desktop Links -->
+      <div class="nav-links">
         <a href="./index.php">Home</a>
         <a href="./gallary.php">Cars</a>
         <a href="./about.php">About</a>
         <a href="./kontakt.php">Kontakt</a>
+      </div>
+
+      <!-- Mobile Menu Button -->
+      <div class="mobile-menu-btn">
+        <button id="menu-btn">
+          <svg class="menu-icon" fill="none" stroke="white" viewBox="0 0 24 24" width="64" height="64">
+            <path id="menu-icon-path" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+              d="M4 6h16M4 12h16M4 18h16" />
+          </svg>
+        </button>
+      </div>
     </div>
-    <script src="./script/nav.js"></script>
-  </nav>
+  </div>
+
+  <!-- Mobile Menu -->
+  <div id="mobile-menu" class="mobile-menu collapsed">
+    <a href="./index.php">Home</a>
+    <a href="./gallary.php">Cars</a>
+    <a href="./about.php">About</a>
+    <a href="./kontakt.php">Kontakt</a>
+  </div>
+  <script src="./script/nav.js"></script>
+</nav>
